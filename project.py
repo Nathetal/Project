36b65b9554439451b0f76d563f625ca169b6a16a
Nathetal <vijay.shah@ashoka.edu.in>
2017-04-27 16:28:50 +0530

    Project v2.0
    Added a function which uses Haversine's formula in order to calculate distance using latitude and longitude for locations where a land route is not possible.

--- project.py
+++ project.py
@@ -1,10 +1,23 @@
 import googlemaps
 import requests.packages.urllib3
+import math
 requests.packages.urllib3.disable_warnings()
 
 api_key="AIzaSyDb40LgQ8lTk5A44CI7GGtjbabgPcfZJWM"
 
 maps_client = googlemaps.Client(key=api_key)
+
+def Absolute_Distance(a,b,c,d):
+	rad_a = math.radians(a)
+	rad_b = math.radians(b)
+	rad_c = math.radians(c)
+	rad_d = math.radians(d)
+	latdiff = rad_b-rad_a
+	longdiff = rad_d-rad_c
+	haversine_a = ((math.sin(latdiff/2))**2) + math.cos(rad_a)*math.cos(rad_b)*((math.sin(longdiff/2))**2)
+	haversine_c = 2*(math.atan2((math.sqrt(haversine_a)), (math.sqrt(1-haversine_a))))
+	haversine_d = 6371 * haversine_c
+	return haversine_d
 
 City1 = str(raw_input("Enter Name of Origin City:"))
 City2 = str(raw_input("Enter Name of Destination City:"))
